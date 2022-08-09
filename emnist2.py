f843c4dd8b0419909cb2e7663ca8e2c8a95d8ba0
tialaron <30555530+tialaron@users.noreply.github.com>
2022-08-09 10:29:01 +0300

    Update emnist2.py

--- emnist2.py
+++ emnist2.py
@@ -2,6 +2,7 @@ import streamlit as st
 from tensorflow.keras.models import load_model
 import numpy as np
 from PIL import Image,  ImageEnhance, ImageFilter #Отрисовка изображений
+import img_preproc
 
 st.set_page_config(layout="wide")
 st.header("Распознавание букв с помощью нейронной сети")
@@ -22,54 +23,7 @@ dictant1 = {0:'0' , 1:'1' , 2:'2' , 3:'3' , 4:'4' , 5:'5' , 6:'6' , 7:'7' , 8:'8
             34:'Y' , 35:'Z' , 36:'a' , 37:'b' , 38:'d' , 39:'e' , 40:'f' , 41:'g' , 42:'h' , 43:'n' , 44:'q' ,
             45:'r' , 46:'t'}
 
-def img_preprocess(img):
-    # To convert PIL Image to numpy array:
-    img_array = np.array(img)
 
-    # Check the type of img_array:
-    # Should output: <class 'numpy.ndarray'>
-    # st.write(type(img_array))
-
-    # Check the shape of img_array:
-    # Should output shape: (height, width, channels)
-    # st.write(img_array.shape)
-
-    # make square shape
-    img_height, img_width = img_array.shape[0], img_array.shape[1]
-    img_center = int(img_width / 2)
-    left_border = int(img_center - img_height / 2)
-    right_border = int(img_center + img_height / 2)
-    img_array1 = img_array[:, left_border:right_border, :]
-
-    # Check the shape of img_array:
-    # st.write(img_array1.shape)
-
-    # convert n save
-    im = Image.fromarray(img_array1)
-    im.save('your_file_image.png')
-    # image11 = Image.open('your_file_image.png')
-    image11 = Image.open('your_file_image.png')
-    img11 = image11.resize((28, 28), Image.ANTIALIAS)
-
-    # convert image to one channel & Numpy array
-    img12 = img11.convert("L")
-    imgData = np.asarray(img12)
-
-    # Calculate THRESHOLD_VALUE
-    # assume dark digit & white sheet
-    step_lobe = .4
-    mid_img_color = np.sum(imgData) / imgData.size
-    min_img_color = imgData.min()
-
-    THRESHOLD_VALUE = int(mid_img_color - (mid_img_color - min_img_color) * step_lobe)
-
-    print(mid_img_color)
-    print(min_img_color)
-    print(THRESHOLD_VALUE)
-
-    thresholdedData = (imgData < THRESHOLD_VALUE) * 1.0
-    imgData1 = np.expand_dims(thresholdedData, axis=0)
-    return imgData1
 
 col21 , col22 = st.columns(2)
 with col21:
