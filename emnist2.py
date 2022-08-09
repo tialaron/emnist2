c9f29bd8b12d05d77858b573eea91a6eb66d7943
tialaron <30555530+tialaron@users.noreply.github.com>
2022-08-09 10:31:47 +0300

    Update emnist2.py

--- emnist2.py
+++ emnist2.py
@@ -15,7 +15,7 @@ testovic = np.load('test.npz')
 X_test_ = testovic['x']
 x_test_2D = X_test_ / 255 # делим на 255, чтобы диапазон был от 0 до 1
 
-model_2d = load_model('/app/emnist1/model_emnist.h5')
+model_2d = load_model('/app/emnist2/model_emnist.h5')
 
 dictant1 = {0:'0' , 1:'1' , 2:'2' , 3:'3' , 4:'4' , 5:'5' , 6:'6' , 7:'7' , 8:'8' , 9:'9' , 10:'A' , 11:'B' ,
             12:'C' , 13:'D' , 14:'E' , 15:'F' , 16:'G' , 17:'H' , 18:'I' , 19:'i' , 20:'k' , 21:'l' , 22:'M' ,
